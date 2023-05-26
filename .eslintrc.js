d9ee9bd945eb834762980aea0cee31d8bc7301a8
Luke Liverpool <lliverpool86@gmail.com>
2023-05-26 15:20:28 -0400

    updated eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
     "env": {
         "browser": true,
-        "commonjs": true,
         "es2021": true
     },
     "extends": "eslint:recommended",
     "overrides": [
     ],
     "parserOptions": {
-        "ecmaVersion": "latest"
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
     "rules": {
     }
